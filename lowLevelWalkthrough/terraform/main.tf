64fc5b705fd69eec744389a0a15a5d97b036e4f8
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-01-25 09:46:43 +0100

    added role and policy

--- lowLevelWalkthrough/terraform/main.tf
+++ lowLevelWalkthrough/terraform/main.tf
@@ -22,3 +22,51 @@ resource "aws_s3_bucket_object" "test-file" {
   content = "Hello from s3"
 }
 
+resource "aws_iam_role" "test-role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::738063116313:role/eks-hellman-kiam-server"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_policy" "test-policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*",
+                "s3:Describe*"
+            ],
+            "Resource": "arn:aws:s3:::dfds-k8sworkshop-bucket"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = "${aws_iam_role.test-role.name}"
+  policy_arn = "${aws_iam_policy.test-policy.arn}"
+}
