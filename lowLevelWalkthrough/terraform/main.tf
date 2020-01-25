7d6a62eaddf4b992f29892a8c984b9a114605f2c
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-01-25 09:50:46 +0100

    corrected typo with type cast

--- lowLevelWalkthrough/terraform/main.tf
+++ lowLevelWalkthrough/terraform/main.tf
@@ -67,6 +67,6 @@ EOF
 
 resource "aws_iam_policy_attachment" "test-attach" {
   name       = "test-attachment"
-  roles      = "${aws_iam_role.test-role.name}"
+  roles      = ["${aws_iam_role.test-role.name}"]
   policy_arn = "${aws_iam_policy.test-policy.arn}"
 }
