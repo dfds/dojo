c3c12fb66d48f529caf8c7b3afbb1b1c63397dba
toban@dfds.com <toban@dfds.com>
2020-09-29 13:43:21 +0200

    =Changed event handlers to fit customElements.define API. Added feedback slide to ppt.

--- workshops/micro-frontend-architecture-deep-dive/katas/3/final/custom-element.js
+++ workshops/micro-frontend-architecture-deep-dive/katas/3/final/custom-element.js
@@ -1,18 +1,5 @@
 class CustomElement extends HTMLElement {
-    // Fires when an instance of the element is created.
-    createdCallback() {
-        console.log('Created', this);
-    };
-    // Fires when an instance was inserted into the document.
-    attachedCallback() {
-        console.log('Attached', this);
-    };
-    // Fires when an instance was removed from the document.
-    detachedCallback() {
-        console.log('Detatched', this);
-    };
-    // Fires when an attribute was added, removed, or updated.
-    attributeChangedCallback(attr, oldVal, newVal) {
-        console.log('Attribute changed', attr, oldVal, newVal);
+    connectedCallback() {
+        console.log('Connected', this);
     };
 }
